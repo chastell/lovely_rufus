dee61433fce56ba7aaa3bcae7733444d5fc36a0c
Piotr Szotkowski <chastell@chastell.net>
2010-11-01 16:35:44 +0100

    really wrappin’ something

--- lib/lovely-rufus/executable.rb
+++ lib/lovely-rufus/executable.rb
@@ -1,7 +1,7 @@
 module LovelyRufus class Executable
 
   def run input = $stdin, output = $stdout
-    output.puts input.read
+    output.puts input.read.gsub(/(.{1,72})( |$\n?)/, "\\1\n")
   end
 
 end end
--- spec/lovely-rufus/executable_spec.rb
+++ spec/lovely-rufus/executable_spec.rb
@@ -10,6 +10,13 @@ module LovelyRufus describe Executable do
       output.read.should == "a short string\n"
     end
 
+    it 'wraps long input to 72 characters by default' do
+      input = StringIO.new 'The Ballyshannon foundered off the the coast of Cariboo, and down in fathoms many went the captain and the crew.'
+      Executable.new.run input, output = StringIO.new
+      output.rewind
+      output.read.should == "The Ballyshannon foundered off the the coast of Cariboo, and down in\nfathoms many went the captain and the crew.\n"
+    end
+
   end
 
 end end
