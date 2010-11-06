4cfb530f4b8cd31a56955887af21a9ffb31552ec
Piotr Szotkowski <chastell@chastell.net>
2010-11-06 18:58:39 +0100

    add parens to method calls

--- lib/lovely-rufus/wrapper.rb
+++ lib/lovely-rufus/wrapper.rb
@@ -7,19 +7,19 @@ module LovelyRufus
     NBSP = ' '
 
     def initialize(text)
-      @paras = text.split "\n\n"
+      @paras = text.split("\n\n")
     end
 
     def wrapped(max_width = 72)
       @paras.map do |para|
-        best_wrap = wrap_para_to_width para, max_width
-        (max_width - 1).downto 1 do |width|
-          shorter = wrap_para_to_width para, width
+        best_wrap = wrap_para_to_width(para, max_width)
+        (max_width - 1).downto(1) do |width|
+          shorter = wrap_para_to_width(para, width)
           break if shorter.count("\n") > best_wrap.count("\n")
           best_wrap = shorter
         end
         best_wrap
-      end.join "\n\n"
+      end.join("\n\n")
     end
 
     private
--- spec/lovely-rufus/executable_spec.rb
+++ spec/lovely-rufus/executable_spec.rb
@@ -10,7 +10,7 @@ module LovelyRufus
         wrapper = mock Wrapper
         Wrapper.should_receive(:new).with('some text').and_return wrapper
         wrapper.should_receive(:wrapped).and_return 'wrapped text'
-        Executable.new.run StringIO.new('some text'), output = StringIO.new
+        Executable.new.run(StringIO.new('some text'), output = StringIO.new)
         output.rewind
         output.read.should == "wrapped text\n"
       end
