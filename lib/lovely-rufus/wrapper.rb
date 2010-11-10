9510b4f8091f42632182827a0673728458bb6465
Piotr Szotkowski <chastell@chastell.net>
2010-11-10 16:52:06 +0100

    back (?) to sanity (‽)

--- lib/lovely-rufus/wrapper.rb
+++ lib/lovely-rufus/wrapper.rb
@@ -26,7 +26,14 @@ module LovelyRufus
 
     def wrap_para_to_width(para, width)
       leader = para.lines.first[/^(> )?/]
-      para.lines.map { |line| line[leader.size..-1] }.join.tr("\n", ' ').gsub(/ ([^ ]) /, " \\1#{NBSP}").gsub(/(.{1,#{width - leader.size}})( |$\n?)/, "\\1\n").lines.map { |line| line.insert(0, leader) }.join.tr(NBSP, ' ').chomp
+      para
+        .lines.map { |line| line[leader.size..-1] }.join
+        .tr("\n", ' ')
+        .gsub(/ ([^ ]) /, " \\1#{NBSP}")
+        .gsub(/(.{1,#{width - leader.size}})( |$\n?)/, "\\1\n")
+        .lines.map { |line| line.insert(0, leader) }.join
+        .tr(NBSP, ' ')
+        .chomp
     end
 
   end
