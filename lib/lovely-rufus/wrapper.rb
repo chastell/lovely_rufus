bd636d7f6d8ab76e2d8403dfe55da1c0cbd8cf32
Piotr Szotkowski <chastell@chastell.net>
2010-11-19 13:42:11 +0100

    teach Wrapper about hangouts

--- lib/lovely-rufus/wrapper.rb
+++ lib/lovely-rufus/wrapper.rb
@@ -16,6 +16,7 @@ module LovelyRufus
         (max_width - 1).downto(1) do |width|
           shorter = wrap_para_to_width(para, width)
           break if shorter.lines.count > best_wrap.lines.count
+          break if shorter.lines.map(&:size).max > best_wrap.lines.map(&:size).max
           best_wrap = shorter
         end
         best_wrap
@@ -23,6 +24,22 @@ module LovelyRufus
     end
 
     private
+
+    def remove_hangouts(para, width)
+      lines = para.split("\n")
+
+      hangout_line = lines.each.with_index do |line, i|
+        next unless space = line.rindex(/[ #{NBSP}]/) and i < lines.size - 1
+        break i if i > 0 and space >= lines[i - 1].size
+        break i if           space >= lines[i + 1].size
+      end
+
+      if hangout_line.is_a?(Integer)
+        lines[hangout_line] << NBSP
+        fixed = lines.join(' ').gsub("#{NBSP} ", NBSP)
+        para.replace(wrap_para_to_width(fixed, width))
+      end
+    end
 
     def wrap_para_to_width(para, width)
       quotes = para[/^([> ]*)/]
@@ -33,6 +50,7 @@ module LovelyRufus
         .tr("\n", ' ')                                    # unwrap para
         .gsub(/ ([^ ]) /, " \\1#{NBSP}")                  # glue 1-letter words
         .gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")         # wrap to width
+        .tap { |para| remove_hangouts(para, width) }      # handle hangouts
         .lines.map { |line| line.insert(0, leader) }.join # re-insert leader
         .tr(NBSP, ' ')                                    # drop glue spaces
         .chomp                                            # final touch
