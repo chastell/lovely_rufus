5daac06c44da9a902b0babba315e62b2cf8a1faa
Piotr Szotkowski <chastell@chastell.net>
2011-09-06 13:40:54 +0200

    Executable spec: use just the first result of capture_io

--- spec/lovely-rufus/executable_spec.rb
+++ spec/lovely-rufus/executable_spec.rb
@@ -5,35 +5,27 @@ require_relative '../spec_helper'
 module LovelyRufus describe Executable do
 
   describe '.new' do
-
     it 'parses passed params to fetch desired width' do
-      out, _ = capture_io do
+      capture_io do
         Executable.new.run StringIO.new 'some text'
-      end
-      out.must_equal "some text\n"
+      end.first.must_equal "some text\n"
 
-      out, _ = capture_io do
+      capture_io do
         Executable.new(['-w', '4']).run StringIO.new 'some text'
-      end
-      out.must_equal "some\ntext\n"
+      end.first.must_equal "some\ntext\n"
 
-      out, _ = capture_io do
+      capture_io do
         Executable.new(['--width', '4']).run StringIO.new 'some text'
-      end
-      out.must_equal "some\ntext\n"
+      end.first.must_equal "some\ntext\n"
     end
-
   end
 
   describe '#run' do
-
     it 'outputs properly wrapped input' do
-      out, _ = capture_io do
+      capture_io do
         Executable.new(['-w', '4']).run StringIO.new 'some more text'
-      end
-      out.must_equal "some\nmore\ntext\n"
+      end.first.must_equal "some\nmore\ntext\n"
     end
-
   end
 
 end end
