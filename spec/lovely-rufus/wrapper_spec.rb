445bd35dd285e2db0a7012dc3e86cdb45b46c8cd
Piotr Szotkowski <chastell@chastell.net>
2013-03-05 09:08:33 +0100

    wrap all the specs

--- spec/lovely-rufus/wrapper_spec.rb
+++ spec/lovely-rufus/wrapper_spec.rb
@@ -6,12 +6,15 @@ module LovelyRufus describe Wrapper do
   describe '#wrapped' do
     it 'returns properly wrapped input' do
       YAML.load_file('spec/fixtures/wrapper.yml').each do |wrapping|
-        Wrapper.new(wrapping['input']).wrapped(wrapping['width'] || 72).must_equal wrapping['output']
+        width   = wrapping.fetch('width') { 72 }
+        wrapped = Wrapper.new(wrapping['input']).wrapped width
+        wrapped.must_equal wrapping['output']
       end
     end
 
     it 'silently strips trailing newlines' do
-      Wrapper.new("a problematic example\n").wrapped(13).must_equal "a problematic\nexample"
+      wrapped = Wrapper.new("a problematic example\n").wrapped 13
+      wrapped.must_equal "a problematic\nexample"
     end
 
     it 'doesn’t breeak on email-quoted single words' do
