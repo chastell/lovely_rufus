b7c4623ec167babbfde6fdf3cce79c93f26bfbf2
Piotr Szotkowski <chastell@chastell.net>
2013-12-06 10:06:34 +0100

    refactor Executable spec

--- spec/lovely-rufus/executable_spec.rb
+++ spec/lovely-rufus/executable_spec.rb
@@ -1,27 +1,25 @@
 require_relative '../spec_helper'
 
 module LovelyRufus describe Executable do
-  describe '.new' do
-    it 'parses passed params to fetch desired width' do
-      capture_io do
-        Executable.new.run StringIO.new 'some text'
-      end.first.must_equal "some text\n"
-
-      capture_io do
-        Executable.new(%w[-w 4]).run StringIO.new 'some text'
-      end.first.must_equal "some\ntext\n"
-
-      capture_io do
-        Executable.new(%w[--width 4]).run StringIO.new 'some text'
-      end.first.must_equal "some\ntext\n"
-    end
-  end
-
   describe '#run' do
+    it 'outputs passed text' do
+      text = StringIO.new 'some text'
+      -> { Executable.new.run text }.must_output "some text\n"
+    end
+
+    it 'wraps passed text to desired width' do
+      text = StringIO.new 'some text'
+      -> { Executable.new(%w[--width 4]).run text }.must_output "some\ntext\n"
+    end
+
+    it 'supports short arguments' do
+      text = StringIO.new 'some text'
+      -> { Executable.new(%w[-w4]).run text }.must_output "some\ntext\n"
+    end
+
     it 'outputs properly wrapped input' do
-      capture_io do
-        Executable.new(%w[-w 4]).run StringIO.new 'some more text'
-      end.first.must_equal "some\nmore\ntext\n"
+      text = StringIO.new 'some more text'
+      -> { Executable.new(%w[-w4]).run text }.must_output "some\nmore\ntext\n"
     end
   end
 end end
