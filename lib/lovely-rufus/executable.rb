b307fa5e899b39736bfe63e3e659b83aa2049f91
Piotr Szotkowski <chastell@chastell.net>
2013-12-11 09:06:33 +0100

    Executable: factor out argument parsing to settings

--- lib/lovely-rufus/executable.rb
+++ lib/lovely-rufus/executable.rb
@@ -1,17 +1,25 @@
 module LovelyRufus class Executable
   def initialize args = ARGV
-    @width = 72
-    OptionParser.new do |opts|
-      opts.on '-w', '--width WIDTH', Integer, 'Wrapping width' do |width|
-        @width = width
-      end
-    end.parse! args
+    @args = args
   end
 
   def run input = $stdin
-    puts Wrapper.new(input.read).wrapped width
+    puts Wrapper.new(input.read).wrapped settings.width
   end
 
-  attr_reader :width
-  private     :width
+  attr_reader :args
+  private     :args
+
+  private
+
+  def settings
+    Struct.new(:width).new.tap do |settings|
+      settings.width = 72
+      OptionParser.new do |opts|
+        opts.on '-w', '--width WIDTH', Integer, 'Wrapping width' do |width|
+          settings.width = width
+        end
+      end.parse! args
+    end
+  end
 end end
