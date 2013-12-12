2138361432877aab964e0e421fdebbe583347638
Piotr Szotkowski <chastell@chastell.net>
2013-12-12 10:25:08 +0100

    start moving over from Wrapper to Wrapper::Para

--- lib/lovely-rufus/wrapper.rb
+++ lib/lovely-rufus/wrapper.rb
@@ -9,7 +9,7 @@ module LovelyRufus class Wrapper
     return '' if paras.all?(&:empty?)
 
     paras.map do |para|
-      wrap_para_recursively para, max_width
+      para.wrap_para_recursively para, max_width
     end.join "\n\n"
   end
 
@@ -19,62 +19,61 @@ module LovelyRufus class Wrapper
   private
 
   class Para < SimpleDelegator
-  end
-
-  def find_hangout_line lines
-    lines.find.with_index do |line, i|
-      i < lines.size - 1 and
-      space = hangout_last_space(line) and
-      (hangout_to_previous_line i, lines, space or
-       hangout_to_next_line i, lines, space)
+    def find_hangout_line lines
+      lines.find.with_index do |line, i|
+        i < lines.size - 1 and
+        space = hangout_last_space(line) and
+        (hangout_to_previous_line i, lines, space or
+        hangout_to_next_line i, lines, space)
+      end
     end
-  end
 
-  def hangout_last_space line
-    line.rindex(/[ #{NBSP}]/)
-  end
-
-  def hangout_to_next_line i, lines, space
-    (i < lines.size - 2 or lines.size == 2) and space + 1 >= lines[i + 1].size
-  end
-
-  def hangout_to_previous_line i, lines, space
-    i > 0 and space + 1 >= lines[i - 1].size
-  end
-
-  def remove_hangouts para, width
-    lines = para.split "\n"
-    hangout_line = find_hangout_line lines
-    if hangout_line
-      hangout_line << NBSP
-      fixed = lines.join(' ').gsub "#{NBSP} ", NBSP
-      para.replace wrap_para_to_width fixed, width
+    def hangout_last_space line
+      line.rindex(/[ #{NBSP}]/)
     end
-  end
 
-  def wrap_para_recursively para, max_width
-    best = wrap_para_to_width para, max_width
-    (max_width - 1).downto 1 do |width|
-      shorter = wrap_para_to_width para, width
-      break if shorter.lines.count           > best.lines.count
-      break if shorter.lines.map(&:size).max > best.lines.map(&:size).max
-      best = shorter
+    def hangout_to_next_line i, lines, space
+      (i < lines.size - 2 or lines.size == 2) and space + 1 >= lines[i + 1].size
     end
-    best
-  end
 
-  def wrap_para_to_width para, width
-    quotes = para[/^([\/#> ]*)/]
-    leader = quotes.empty? ? '' : quotes.tr(' ', '') + ' '
-    width -= leader.size if width > leader.size
-    para
-      .lines.map { |line| line[quotes.size..-1] }.join  # drop quotes
-      .tr("\n", ' ')                                    # unwrap para
-      .gsub(/ ([^ ]) /, " \\1#{NBSP}")                  # glue 1-letter words
-      .gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")         # wrap to width
-      .tap { |par| remove_hangouts par, width }         # handle hangouts
-      .lines.map { |line| line.insert 0, leader }.join  # re-insert leader
-      .tr(NBSP, ' ')                                    # drop glue spaces
-      .chomp                                            # final touch
+    def hangout_to_previous_line i, lines, space
+      i > 0 and space + 1 >= lines[i - 1].size
+    end
+
+    def remove_hangouts para, width
+      lines = para.split "\n"
+      hangout_line = find_hangout_line lines
+      if hangout_line
+        hangout_line << NBSP
+        fixed = lines.join(' ').gsub "#{NBSP} ", NBSP
+        para.replace wrap_para_to_width fixed, width
+      end
+    end
+
+    def wrap_para_recursively para, max_width
+      best = wrap_para_to_width para, max_width
+      (max_width - 1).downto 1 do |width|
+        shorter = wrap_para_to_width para, width
+        break if shorter.lines.count           > best.lines.count
+        break if shorter.lines.map(&:size).max > best.lines.map(&:size).max
+        best = shorter
+      end
+      best
+    end
+
+    def wrap_para_to_width para, width
+      quotes = para[/^([\/#> ]*)/]
+      leader = quotes.empty? ? '' : quotes.tr(' ', '') + ' '
+      width -= leader.size if width > leader.size
+      para
+        .lines.map { |line| line[quotes.size..-1] }.join  # drop quotes
+        .tr("\n", ' ')                                    # unwrap para
+        .gsub(/ ([^ ]) /, " \\1#{NBSP}")                  # glue 1-letter words
+        .gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")         # wrap to width
+        .tap { |par| remove_hangouts par, width }         # handle hangouts
+        .lines.map { |line| line.insert 0, leader }.join  # re-insert leader
+        .tr(NBSP, ' ')                                    # drop glue spaces
+        .chomp                                            # final touch
+    end
   end
 end end
