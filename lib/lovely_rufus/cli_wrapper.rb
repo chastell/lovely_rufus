c2fd1f85cbdb4c28f494e70a4f0453984651f3b9
Piotr Szotkowski <chastell@chastell.net>
2013-12-19 09:04:04 +0100

    CLIWrapper#wrap: actually start wrapping

--- lib/lovely_rufus/cli_wrapper.rb
+++ lib/lovely_rufus/cli_wrapper.rb
@@ -1,5 +1,5 @@
 module LovelyRufus class CLIWrapper
   def wrap input
-    input
+    input.gsub(/(.{1,72})( |$\n?)/, "\\1\n").chomp
   end
 end end
--- spec/lovely_rufus/cli_wrapper_spec.rb
+++ spec/lovely_rufus/cli_wrapper_spec.rb
@@ -2,9 +2,12 @@ require_relative '../spec_helper'
 
 module LovelyRufus describe CLIWrapper do
   describe '#wrap' do
-    it 'wraps the passed String to 72 characters' do
-      CLIWrapper.new.wrap('all right: stop, collaborate and listen')
-        .must_equal 'all right: stop, collaborate and listen'
+    it 'wraps the passed String to 72 characters by default' do
+      short = 'all right: stop, collaborate and listen'
+      CLIWrapper.new.wrap(short).must_equal short
+      long = short + ' – Ice is back with a brand new invention'
+      wrap = short + " – Ice is back with a brand new\ninvention"
+      CLIWrapper.new.wrap(long).must_equal wrap
     end
   end
 end end
