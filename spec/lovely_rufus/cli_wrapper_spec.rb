6632df3ef2d81af21b748f472d513ed9105d9886
Piotr Szotkowski <chastell@chastell.net>
2013-12-24 15:16:56 +0100

    refactor CLIWrapper spec a bit

--- spec/lovely_rufus/cli_wrapper_spec.rb
+++ spec/lovely_rufus/cli_wrapper_spec.rb
@@ -2,19 +2,18 @@ require_relative '../spec_helper'
 
 module LovelyRufus describe CLIWrapper do
   describe '#run' do
+    let(:text) { "all right: stop, collaborate and listen\n" }
+    let(:text_wrapper) { fake :text_wrapper, as: :class }
+
     it 'reads the passed stream to TextWrapper and prints the results' do
-      text = 'all right: stop, collaborate and listen'
-      text_wrapper = fake :text_wrapper, as: :class
       stub(text_wrapper).wrap(text, width: 72) { text }
       lambda do
         CLIWrapper.new(text_wrapper: text_wrapper).run StringIO.new text
-      end.must_output "all right: stop, collaborate and listen\n"
+      end.must_output text
     end
 
     it 'accepts the desired width and passes it to TextWrapper' do
-      text = 'all right: stop, collaborate and listen'
       wrap = "all right: stop,\ncollaborate and listen\n"
-      text_wrapper = fake :text_wrapper, as: :class
       stub(text_wrapper).wrap(text, width: 22) { wrap }
       lambda do
         stream = StringIO.new text
