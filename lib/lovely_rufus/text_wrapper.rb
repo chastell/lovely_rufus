2df92a2ad593dcaf376def626edb8fc5c025067d
Piotr Szotkowski <chastell@chastell.net>
2013-12-28 10:37:40 +0100

    TextWrapper: switch to instance-based approach

--- lib/lovely_rufus/text_wrapper.rb
+++ lib/lovely_rufus/text_wrapper.rb
@@ -1,14 +1,25 @@
 module LovelyRufus class TextWrapper
   def self.wrap text, width: 72
+    new(text, width: width).wrapped
+  end
+
+  def initialize text, width: 72
+    @text, @width = text, width
+  end
+
+  def wrapped
     best = wrap_text_to text, width
     (1..(width - 1)).map { |len| wrap_text_to best, len }.find do |shorter|
       shorter.lines.count <= best.lines.count
     end or best
   end
 
+  attr_reader :text, :width
+  private     :text, :width
+
   private
 
-  def self.wrap_text_to text, width
+  def wrap_text_to text, width
     text.tr("\n", ' ').strip.gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")
   end
 end end
