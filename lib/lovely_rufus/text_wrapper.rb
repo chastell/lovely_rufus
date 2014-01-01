a45b435a7a8af41bbc7e83a2016e303f3758dae5
Piotr Szotkowski <chastell@chastell.net>
2014-01-01 11:48:28 +0100

    TextWrapper: factor out HangoutFinder

--- lib/lovely_rufus/text_wrapper.rb
+++ lib/lovely_rufus/text_wrapper.rb
@@ -20,17 +20,22 @@ module LovelyRufus class TextWrapper
   def wrap_to size
     wrapped = text.gsub(/(.{1,#{size}})( |$\n?)/, "\\1\n")
     lines   = wrapped.lines.map(&:chomp)
-    hangout = lines.each_index.find do |i|
-      next unless pos = lines[i].rindex(/\p{Space}/)
-      to_prev = i > 0 && pos >= lines[i - 1].size
-      to_next = i < lines.size - 1 && pos >= lines[i + 1].size
-      to_prev or to_next
-    end
-    if hangout
+    if hangout = HangoutFinder.hangout_line(lines)
       lines[hangout] << ' '
       fixed = lines.join(' ').gsub('  ', ' ')
       wrapped = fixed.gsub(/(.{1,#{size}})( |$\n?)/, "\\1\n")
     end
     wrapped
+  end
+
+  class HangoutFinder
+    def self.hangout_line lines
+      lines.each_index.find do |i|
+        next unless pos = lines[i].rindex(/\p{Space}/)
+        to_prev = i > 0 && pos >= lines[i - 1].size
+        to_next = i < lines.size - 1 && pos >= lines[i + 1].size
+        to_prev or to_next
+      end
+    end
   end
 end end
