5618f0ef564929fbde2b1280b97f39045926e76b
Piotr Szotkowski <chastell@chastell.net>
2014-01-15 19:49:49 +0100

    OneLetterGluer: indentation fix

--- lib/lovely_rufus/one_letter_gluer.rb
+++ lib/lovely_rufus/one_letter_gluer.rb
@@ -1,12 +1,12 @@
 module LovelyRufus class OneLetterGluer
-    def initialize text, width: 72
-      @text = text
-    end
+  def initialize text, width: 72
+    @text = text
+  end
 
-    def call
-      text.gsub(/ ([^ ]) /, ' \\1 ')
-    end
+  def call
+    text.gsub(/ ([^ ]) /, ' \\1 ')
+  end
 
-    attr_reader :text
-    private     :text
+  attr_reader :text
+  private     :text
 end end
