f6cf82b55cf3b19bff3c9284980440bc9bcb12ac
Piotr Szotkowski <chastell@chastell.net>
2014-01-17 09:41:44 +0100

    make BasicWrapper spec more readable

--- spec/lovely_rufus/basic_wrapper_spec.rb
+++ spec/lovely_rufus/basic_wrapper_spec.rb
@@ -5,9 +5,11 @@ module LovelyRufus describe BasicWrapper do
     it 'wraps text to the given width' do
       text = 'I go crazy when I hear a cymbal and a hi-hat ' +
         'with a souped-up tempo'
-      BasicWrapper.new.call(text, width: 22)
-        .must_equal "I go crazy when I hear\n" +
-        "a cymbal and a hi-hat\n" + "with a souped-up tempo\n"
+      BasicWrapper.new.call(text, width: 22).must_equal <<-end.dedent
+        I go crazy when I hear
+        a cymbal and a hi-hat
+        with a souped-up tempo
+      end
     end
   end
 end end
