c644d32f82b2d3269aa5985475b0999160f2cd85
Piotr Szotkowski <chastell@chastell.net>
2014-01-17 09:41:32 +0100

    HangoutWrapper: simplify a bit

--- lib/lovely_rufus/hangout_wrapper.rb
+++ lib/lovely_rufus/hangout_wrapper.rb
@@ -1,6 +1,6 @@
 module LovelyRufus class HangoutWrapper
   def call text, width: 72
-    @text = text
+    @lines = text.lines.map(&:chomp)
     return lines.join("\n") + "\n" unless hangout_line
     hangout_line << "\u1FFF"
     unfolded = lines.join(' ').gsub("\u1FFF ", "\u1FFF")
@@ -8,8 +8,8 @@ module LovelyRufus class HangoutWrapper
     HangoutWrapper.new.call(wrapped, width: width).gsub("\u1FFF", ' ')
   end
 
-  attr_reader :text
-  private     :text
+  attr_reader :lines
+  private     :lines
 
   private
 
@@ -18,9 +18,5 @@ module LovelyRufus class HangoutWrapper
       return a if a[/\p{Space}/] and a.rindex(/\p{Space}/) >= b.size
       return b if b[/\p{Space}/] and b.rindex(/\p{Space}/) >= a.size
     end
-  end
-
-  def lines
-    @lines ||= text.lines.map(&:chomp)
   end
 end end
