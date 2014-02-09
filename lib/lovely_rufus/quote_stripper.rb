c72404efc5c3c5f58f35fb57c2c8fa7cb694d5ea
Piotr Szotkowski <chastell@chastell.net>
2014-02-09 11:14:48 +0100

    QuoteStripper: factor out #quote and #stripped_wrap

--- lib/lovely_rufus/quote_stripper.rb
+++ lib/lovely_rufus/quote_stripper.rb
@@ -1,9 +1,22 @@
 module LovelyRufus class QuoteStripper < Layer
   def call wrap
-    quote = wrap.text.lines.all? { |line| line.start_with? '> ' } ? '> ' : ''
-    stripped = wrap.text.lines.map { |line| line[quote.size..-1] }.join
-    wrapped  = next_layer.call Wrap[stripped, width: wrap.width - quote.size]
-    quoted   = wrapped.text.lines.map { |line| quote + line }.join
+    @wrap   = wrap
+    wrapped = next_layer.call stripped_wrap
+    quoted  = wrapped.text.lines.map { |line| quote + line }.join
     Wrap[quoted, width: wrapped.width + quote.size]
+  end
+
+  attr_reader :wrap
+  private     :wrap
+
+  private
+
+  def quote
+    @quote ||= wrap.text.lines.all? { |l| l.start_with? '> ' } ? '> ' : ''
+  end
+
+  def stripped_wrap
+    stripped_text = wrap.text.lines.map { |line| line[quote.size..-1] }.join
+    Wrap[stripped_text, width: wrap.width - quote.size]
   end
 end end
