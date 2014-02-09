e4228e5ac1991ad005f1b06b30373444b8625796
Piotr Szotkowski <chastell@chastell.net>
2014-02-09 13:40:08 +0100

    QuoteStripper spec: fix capitalisation of examples

--- spec/lovely_rufus/quote_stripper_spec.rb
+++ spec/lovely_rufus/quote_stripper_spec.rb
@@ -4,11 +4,11 @@ module LovelyRufus describe QuoteStripper do
   describe '#call' do
     it 'strips quotes and adjusts width before calling the next layer' do
       quoted = <<-end.dedent
-        > To the extreme I rock a mic like a vandal
+        > to the extreme I rock a mic like a vandal
         > light up a stage and wax a chump like a candle
       end
       unquoted = <<-end.dedent
-        To the extreme I rock a mic like a vandal
+        to the extreme I rock a mic like a vandal
         light up a stage and wax a chump like a candle
       end
       layer = fake :layer, call: Wrap[unquoted, width: 70]
@@ -18,7 +18,7 @@ module LovelyRufus describe QuoteStripper do
 
     it 'adds quotes back in (and adjusts width) before returning' do
       quoted = <<-end.dedent
-        > Take heed, ’cause I’m a lyrical poet
+        > take heed, ’cause I’m a lyrical poet
         > Miami’s on the scene just in case you didn’t know it
       end
       wrap = Wrap[quoted, width: 72]
@@ -27,7 +27,7 @@ module LovelyRufus describe QuoteStripper do
 
     it 'does not touch non-quoted texts' do
       plain = <<-end.dedent
-        My town, that created all the bass sound
+        my town, that created all the bass sound
         enough to shake and kick holes in the ground
       end
       wrap = Wrap[plain, width: 72]
@@ -41,12 +41,12 @@ module LovelyRufus describe QuoteStripper do
 
     it 'strips multilevel quotes' do
       quoted = <<-end.dedent
-        >> ’Cause my style’s like a chemical spill
-        >> Feasible rhymes that you can vision and feel
+        >> ’cause my style’s like a chemical spill
+        >> feasible rhymes that you can vision and feel
       end
       unquoted = <<-end.dedent
-        ’Cause my style’s like a chemical spill
-        Feasible rhymes that you can vision and feel
+        ’cause my style’s like a chemical spill
+        feasible rhymes that you can vision and feel
       end
       layer = fake :layer, call: Wrap[unquoted, width: 69]
       QuoteStripper.new(layer).call Wrap[quoted, width: 72]
