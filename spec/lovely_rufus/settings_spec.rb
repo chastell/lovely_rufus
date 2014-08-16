e92ed718552029d2b7841f386ed46733cdb35634
Piotr Szotkowski <chastell@chastell.net>
2014-08-16 23:12:09 +0200

    spec Settings#width

---
+++ spec/lovely_rufus/settings_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../spec_helper'
+require_relative '../../lib/lovely_rufus/settings'
+
+module LovelyRufus
+  describe Settings do
+    describe '#width' do
+      it 'defaults to 72' do
+        Settings.new([]).width.must_equal 72
+      end
+
+      it 'can be set via --width' do
+        Settings.new(%w(--width=42)).width.must_equal 42
+      end
+
+      it 'can be set via -w' do
+        Settings.new(%w(-w 42)).width.must_equal 42
+      end
+    end
+  end
+end
