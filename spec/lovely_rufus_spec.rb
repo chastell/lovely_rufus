83e244c7d7f0a1c8aaf7ac2e4299e287134c7928
Piotr Szotkowski <chastell@chastell.net>
2014-10-04 01:56:38 +0200

    LovelyRufus.wrap spec: drop stuff tested in TextWrapper.wrap specs

--- spec/lovely_rufus_spec.rb
+++ spec/lovely_rufus_spec.rb
@@ -16,38 +16,5 @@ describe LovelyRufus do
       LovelyRufus.wrap 'Ice Ice Baby', text_wrapper: text_wrapper
       text_wrapper.must_have_received :wrap, ['Ice Ice Baby', { width: 72 }]
     end
-
-    it 'wraps the passed String to the given number of characters' do
-      input = 'something grabs a hold of me tightly; ' \
-        'flow like a harpoon – daily and nightly'
-      LovelyRufus.wrap(input, width: 40).must_equal <<-end.dedent
-        something grabs a hold of me tightly;
-        flow like a harpoon – daily and nightly
-      end
-      LovelyRufus.wrap(input, width: 21).must_equal <<-end.dedent
-        something grabs
-        a hold of me tightly;
-        flow like a harpoon
-        – daily and nightly
-      end
-    end
-
-    it 'rewraps a String from zero' do
-      broken = <<-end.dedent
-        turn off
-        the lights and I’ll glow
-      end
-      wrapped = LovelyRufus.wrap(broken)
-      wrapped.must_equal "turn off the lights and I’ll glow\n"
-    end
-
-    it 'supports all the example use-cases' do
-      path = File.expand_path('lovely_rufus/text_wrapper_spec.yml', __dir__)
-      YAML.load_file(path).each do |spec|
-        width = spec.fetch('width') { 72 }
-        wrap  = "#{spec['output']}\n"
-        LovelyRufus.wrap(spec['input'], width: width).must_equal wrap
-      end
-    end
   end
 end
