dbc666f9d231cb3d1b71e8eff2b3773ba74dbabc
Piotr Szotkowski <chastell@chastell.net>
2014-10-05 21:52:25 +0200

    LovelyRufus.wrap spec: factor out text_wrapper

--- spec/lovely_rufus_spec.rb
+++ spec/lovely_rufus_spec.rb
@@ -4,15 +4,15 @@ require_relative '../lib/lovely_rufus'
 
 describe LovelyRufus do
   describe '.wrap' do
+    let(:text_wrapper) { fake(:text_wrapper, as: :class) }
+
     it 'offloads the wrapping to TextWrapper' do
-      text_wrapper = fake(:text_wrapper, as: :class)
       stub(text_wrapper).wrap('Ice Ice Baby', width: 7) { "Ice Ice\nBaby" }
       LovelyRufus.wrap('Ice Ice Baby', text_wrapper: text_wrapper, width: 7)
         .must_equal "Ice Ice\nBaby"
     end
 
     it 'wraps the passed String to 72 characters by default' do
-      text_wrapper = fake(:text_wrapper, as: :class)
       LovelyRufus.wrap 'Ice Ice Baby', text_wrapper: text_wrapper
       text_wrapper.must_have_received :wrap, ['Ice Ice Baby', { width: 72 }]
     end
