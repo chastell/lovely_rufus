18e3a9bfbb55586c9c222f3aa988584558eb0349
Piotr Szotkowski <chastell@chastell.net>
2014-11-15 17:43:41 +0100

    factor out HangoutWrapper::HangoutFinder.reverse?

--- lib/lovely_rufus/hangout_wrapper.rb
+++ lib/lovely_rufus/hangout_wrapper.rb
@@ -22,25 +22,25 @@ module LovelyRufus
         last_space = line_a.chomp.rindex(/\p{space}/)
         last_space and last_space >= line_b.chomp.size
       end
+
+      def reverse?(line_a, line_b)
+        cut = line_a.chomp.rindex(/\p{space}/)
+        a_after = line_a[0...cut] + "\n"
+        b_after = line_a[(cut + 1)..-1] + line_b
+        b_after.chomp.rindex(/\p{space}/) > a_after.size
+      end
     end
 
     def hangout_line
       lines.each_cons(2).with_index do |(a, b), i|
         if HangoutFinder.between?(a, b)
-          return a unless i == lines.size - 2 and reverse_hangout?(a, b)
+          return a unless i == lines.size - 2 and HangoutFinder.reverse?(a, b)
         end
       end
     end
 
     def lines
       @lines ||= wrap.lines
-    end
-
-    def reverse_hangout?(line_a, line_b)
-      cut = line_a.chomp.rindex(/\p{space}/)
-      a_after = line_a[0...cut] + "\n"
-      b_after = line_a[(cut + 1)..-1] + line_b
-      b_after.chomp.rindex(/\p{space}/) > a_after.size
     end
 
     def rewrapped
