80c5a00587226c8137c452050c4fe286624f59e0
Piotr Szotkowski <chastell@chastell.net>
2015-01-23 00:20:13 +0100

    minor CLIWrapper test refactoring

--- test/lovely_rufus/cli_wrapper_test.rb
+++ test/lovely_rufus/cli_wrapper_test.rb
@@ -5,19 +5,19 @@ require_relative '../../lib/lovely_rufus/cli_wrapper'
 module LovelyRufus
   describe CLIWrapper do
     describe '#run' do
-      let(:text) { "all right: stop, collaborate and listen\n" }
-      let(:text_wrapper) { fake(:text_wrapper, as: :class) }
+      let(:stream)       { StringIO.new(text)                          }
+      let(:text)         { "all right: stop, collaborate and listen\n" }
+      let(:text_wrapper) { fake(:text_wrapper, as: :class)             }
 
       it 'reads the passed stream to TextWrapper and prints the results' do
         stub(text_wrapper).wrap(text, width: 72) { text }
         lambda do
-          CLIWrapper.new(text_wrapper: text_wrapper).run StringIO.new(text)
+          CLIWrapper.new(text_wrapper: text_wrapper).run stream
         end.must_output text
       end
 
       it 'accepts the desired width and passes it to TextWrapper' do
         capture_io do
-          stream = StringIO.new(text)
           CLIWrapper.new(%w(--width=22), text_wrapper: text_wrapper).run stream
         end
         text_wrapper.must_have_received :wrap, [text, { width: 22 }]
