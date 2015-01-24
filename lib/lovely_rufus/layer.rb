67f010d4213f70c0e0aa02a0e0591724e61337ee
Piotr Szotkowski <chastell@chastell.net>
2015-01-24 08:56:02 +0100

    Layer#call: clarify the required parameter

--- lib/lovely_rufus/layer.rb
+++ lib/lovely_rufus/layer.rb
@@ -4,8 +4,8 @@ module LovelyRufus
       @next_layer = next_layer
     end
 
-    def call(_opts = {})
-      fail 'Layer subclasses must define #call'
+    def call(_wrap)
+      fail 'Layer subclasses must define #call that takes a Wrap'
     end
 
     attr_reader :next_layer
