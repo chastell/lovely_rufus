f0bc519942542a99b8f1075d776137b3268c54a1
Piotr Szotkowski <chastell@chastell.net>
2015-02-03 00:13:20 +0100

    make RuboCop even happier

--- test/lovely_rufus/wrap_test.rb
+++ test/lovely_rufus/wrap_test.rb
@@ -5,12 +5,12 @@ module LovelyRufus
   describe Wrap do
     describe '.[]' do
       it 'creates a Wrap with the given text and target width' do
-        Wrap['Ice Ice Baby', width: 7].text.must_equal  'Ice Ice Baby'
+        Wrap['Ice Ice Baby', width: 7].text.must_equal 'Ice Ice Baby'
         Wrap['Ice Ice Baby', width: 7].width.must_equal 7
       end
 
       it 'defaults to empty text and width of 72' do
-        Wrap[].text.must_equal  ''
+        Wrap[].text.must_equal ''
         Wrap[].width.must_equal 72
       end
     end
