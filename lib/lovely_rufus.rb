fe49e1802066838dd51975bc7f6aef48f69b5635
Piotr Szotkowski <chastell@chastell.net>
2015-02-10 02:31:18 +0100

    turn LovelyRufus.wrap into a module_function

--- lib/lovely_rufus.rb
+++ lib/lovely_rufus.rb
@@ -6,7 +6,9 @@ require_relative 'lovely_rufus/text_wrapper'
 module LovelyRufus
   NBSP = "\u00A0"
 
-  def self.wrap(text, text_wrapper: TextWrapper, width: 72)
+  module_function
+
+  def wrap(text, text_wrapper: TextWrapper, width: 72)
     text_wrapper.wrap(text, width: width)
   end
 end
