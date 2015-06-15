666bf8bce271c7a6851c7cd3928e386e653782ef
Piotr Szotkowski <chastell@chastell.net>
2015-06-16 00:43:46 +0200

    port LovelyRufus test to Expectation syntax

--- test/lovely_rufus_test.rb
+++ test/lovely_rufus_test.rb
@@ -7,13 +7,13 @@ describe LovelyRufus do
 
     it 'offloads the wrapping to TextWrapper' do
       stub(text_wrapper).wrap('Ice Ice Baby', width: 7) { "Ice Ice\nBaby\n" }
-      LovelyRufus.wrap('Ice Ice Baby', text_wrapper: text_wrapper, width: 7)
+      _(LovelyRufus.wrap('Ice Ice Baby', text_wrapper: text_wrapper, width: 7))
         .must_equal "Ice Ice\nBaby\n"
     end
 
     it 'wraps the passed String to 72 characters by default' do
       LovelyRufus.wrap 'Ice Ice Baby', text_wrapper: text_wrapper
-      text_wrapper.must_have_received :wrap, ['Ice Ice Baby', { width: 72 }]
+      _(text_wrapper).must_have_received :wrap, ['Ice Ice Baby', { width: 72 }]
     end
   end
 end
