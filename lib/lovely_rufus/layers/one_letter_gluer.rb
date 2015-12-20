179d7552a95221f8bb7d05335169d50e3fa91879
Piotr Szotkowski <chastell@chastell.net>
2015-12-21 00:03:25 +0100

    whitelist OneLetterGluer#call and become Reek-clean!

--- lib/lovely_rufus/layers/one_letter_gluer.rb
+++ lib/lovely_rufus/layers/one_letter_gluer.rb
@@ -4,6 +4,7 @@ require_relative 'layer'
 module LovelyRufus
   module Layers
     class OneLetterGluer < Layer
+      # :reek:FeatureEnvy
       def call(wrap)
         pattern = /(?<=\p{space})(&|\p{letter})\p{space}/
         text    = wrap.text.gsub(pattern, "\\1\\2#{NBSP}")
