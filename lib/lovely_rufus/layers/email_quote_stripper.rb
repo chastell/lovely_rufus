b5e4936c55575c52e1f4e8c8a787d6f17c9d608d
Piotr Szotkowski <chastell@chastell.net>
2016-01-06 14:12:32 +0100

    Reek’s UnusedPrivateMethod skipping is class-level

--- lib/lovely_rufus/layers/email_quote_stripper.rb
+++ lib/lovely_rufus/layers/email_quote_stripper.rb
@@ -2,12 +2,12 @@ require_relative 'quote_stripper'
 
 module LovelyRufus
   module Layers
+    # :reek:UnusedPrivateMethod
     class EmailQuoteStripper < QuoteStripper
       QUOTES = /^>[> ]*/
 
       private
 
-      # :reek:UnusedPrivateMethod
       def fixed_quote
         quote.size > 0 ? quote.delete(' ') + ' ' : ''
       end
