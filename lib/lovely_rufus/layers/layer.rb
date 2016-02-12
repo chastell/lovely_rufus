f6bf2934d26f79bb578e3c5107bcdf8a3c24657c
Piotr Szotkowski <chastell@chastell.net>
2016-02-13 00:49:24 +0100

    RuboCop: fix Style/SignalException

--- lib/lovely_rufus/layers/layer.rb
+++ lib/lovely_rufus/layers/layer.rb
@@ -6,7 +6,7 @@ module LovelyRufus
       end
 
       def call(_wrap)
-        fail 'Layer subclasses must define #call that takes a Wrap'
+        raise 'Layer subclasses must define #call that takes a Wrap'
       end
 
       private
